c6490055d07b919be3c83a428a967f92cb7bad25
Juliana Lobo <sousalobojuliana@gmail.com>
2026-06-10 12:30:16 -0300

    updating base

--- obj/Debug/net10.0/TesteSafariIssueWebassembly.AssemblyInfo.cs
+++ obj/Debug/net10.0/TesteSafariIssueWebassembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteSafariIssueWebassembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19cfecaca77d2ab5e96f534b70bf351409442520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e5f9856892091a1726414b8a9a2c17102ab5ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteSafariIssueWebassembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteSafariIssueWebassembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
